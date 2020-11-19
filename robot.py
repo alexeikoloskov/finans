326de4dd664e219760735b54d07bba43749a2851
alexeikoloskov <alexeikoloskov@inbox.ru>
2020-11-19 21:01:59 +0300

    update

--- robot.py
+++ robot.py
@@ -18,26 +18,29 @@ def round_number(self):
 
 
 class get_instrument():
-    def price_dollar(i=14):  # i - какой доллар в портфеле
+    def price_dollar():  # i - какой доллар в портфеле
         pf = client.portfolio.portfolio_get()
-        balance = pf.payload.positions[i].average_position_price.value
-        WM = pf.payload.positions[i].expected_yield.value
-        how_much = pf.payload.positions[i].balance
-        cb = (balance * how_much) + WM
-        weidth = round(cb / how_much, 3)
-        return weidth
+        baks = pf.payload.positions
+        for i in baks:
+            if i.name == 'Доллар США':
+                balance = i.average_position_price.value
+                WM = i.expected_yield.value
+                how_much = i.balance
+                cb = (balance * how_much) + WM
+                weidth = round(cb / how_much, 3)
+                return weidth
 
     def one(i):
         pf = client.portfolio.portfolio_get()
         print(pf)
-        balance=pf.payload.positions[i].average_position_price.value
-        WM=pf.payload.positions[i].expected_yield.value
+        balance = pf.payload.positions[i].average_position_price.value
+        WM = pf.payload.positions[i].expected_yield.value
         how_much = pf.payload.positions[i].balance
-        b=balance*how_much
-        cb=(balance*how_much)+WM
-        weidth=cb/how_much
-        one_percent=b/100
-        how_percent=round((WM/one_percent),2)
+        b = balance * how_much
+        cb = (balance * how_much) + WM
+        weidth = cb / how_much
+        one_percent = b / 100
+        how_percent = round((WM / one_percent), 2)
         print('Название:', pf.payload.positions[i].name)
         print('Тикер:', pf.payload.positions[i].ticker)
         print('Валюта:', pf.payload.positions[i].average_position_price.currency)
@@ -57,20 +60,20 @@ class get_instrument():
         all_currency = client.portfolio.portfolio_currencies_get().payload.currencies
         len_currencies = int(len(all_currency))
         i = 0
-        while len_currencies >= i+1:
+        while len_currencies >= i + 1:
             currency = all_currency[i]
             if currency.balance == 0.0:
                 continue
             else:
-                print(currency.currency,':', currency.balance)
-            i+=1
+                print(currency.currency, ':', currency.balance)
+            i += 1
 
     @staticmethod
     def all():
         pf = client.portfolio.portfolio_get()
         position = pf.payload.positions
         how_much = int(len(pf.payload.positions))
-        k=0
+        k = 0
         value, currency, balance, ticker, name, amount_all, current_amount, current_amount_all = [], [], [], [], [], [], [], []
         # print(how_much, ' - Общее количество инструментов')
         for i in position:
@@ -87,15 +90,13 @@ class get_instrument():
             ticker.append(i.ticker)
             name.append(i.name)
             aa = i.average_position_price.value * i.balance
-            ca = (i.average_position_price.value*i.balance+i.expected_yield.value)/i.balance
-            caa = i.average_position_price.value*i.balance+i.expected_yield.value
+            ca = (i.average_position_price.value * i.balance + i.expected_yield.value) / i.balance
+            caa = i.average_position_price.value * i.balance + i.expected_yield.value
             amount_all.append(round_number(aa))
             current_amount.append(round_number(ca))
             current_amount_all.append(round_number(caa))
-            k+=1
+            k += 1
         return value, currency, balance, ticker, name, amount_all, current_amount, current_amount_all
-
-
 
 
 # Получить список опреций в портфеле
@@ -111,51 +112,53 @@ class get_instrument():
 # Качаем все операции с 30 сентября 2016
 
 class get_operation():
-    def one(x,zone='Europe/Moscow'):
+    def one(x, zone='Europe/Moscow'):
         d1 = datetime(2016, 9, 30, 0, 0, 0,
                       tzinfo=timezone(zone))  # timezone нужно указывать. Иначе - ошибка
         d2 = datetime.now(tz=timezone(zone))  # По настоящее время
         ops = client.operations.operations_get(_from=d1.isoformat(), to=d2.isoformat())
         # Получить инфо по одной конкретной операции
-        op=ops.payload.operations[x]
+        op = ops.payload.operations[x]
         print('--------------')
-        print(op.figi) # figi всегда берем из операции
-        print(op.operation_type)   # и тип операции тоже
+        print(op.figi)  # figi всегда берем из операции
+        print(op.operation_type)  # и тип операции тоже
         print(op.date)
-        if op.trades == None:      # Если биржевых сделок нет
-            print('price:', op.price)       # Берем из операции цену бумаги
-            print('payment:', op.payment)   # Сумму платежа
-            print('quantity:', op.quantity) # И количество бумаг
+        if op.trades == None:  # Если биржевых сделок нет
+            print('price:', op.price)  # Берем из операции цену бумаги
+            print('payment:', op.payment)  # Сумму платежа
+            print('quantity:', op.quantity)  # И количество бумаг
         else:
-            for t in op.trades:                   # А если есть сделки - то перебираем их
-                print('price:', t.price)          # И берем данные из них
+            for t in op.trades:  # А если есть сделки - то перебираем их
+                print('price:', t.price)  # И берем данные из них
                 print('quantity:', t.quantity)
                 get = client.market.market_search_by_figi_get(op.figi)
                 print(get.payload.name)
         print('--------------')
-    def all(year,month,day=1,hour=0,minute=0,second=1,zone='Europe/Moscow'):
+
+    def all(year, month, day=1, hour=0, minute=0, second=1, zone='Europe/Moscow'):
         # Весь список операций
         d1 = datetime(year, month, day, hour, minute, second,
                       tzinfo=timezone(zone))  # timezone нужно указывать. Иначе - ошибка
         d2 = datetime.now(tz=timezone(zone))  # По настоящее время
         ops = client.operations.operations_get(_from=d1.isoformat(), to=d2.isoformat())
         print('--------------')
-        for op in ops.payload.operations: # Перебираем операции
-            print(op.figi) # figi всегда берем из операции
-            print(op.operation_type)   # и тип операции тоже
+        for op in ops.payload.operations:  # Перебираем операции
+            print(op.figi)  # figi всегда берем из операции
+            print(op.operation_type)  # и тип операции тоже
             print(op.date)
-            if op.trades == None:      # Если биржевых сделок нет
-                print('price:', op.price)       # Берем из операции цену бумаги
-                print('payment:', op.payment)   # Сумму платежа
-                print('quantity:', op.quantity) # И количество бумаг
+            if op.trades == None:  # Если биржевых сделок нет
+                print('price:', op.price)  # Берем из операции цену бумаги
+                print('payment:', op.payment)  # Сумму платежа
+                print('quantity:', op.quantity)  # И количество бумаг
             else:
-                for t in op.trades:                   # А если есть сделки - то перебираем их
-                    print('price:', t.price)          # И берем данные из них
+                for t in op.trades:  # А если есть сделки - то перебираем их
+                    print('price:', t.price)  # И берем данные из них
                     print('quantity:', t.quantity)
                     get = client.market.market_search_by_figi_get(op.figi)
                     print(get.payload.name)
             print('--------------')
-    def in_date(year=2016,month=9,day=1,year2=2016,month2=9,day2=1,zone='Europe/Moscow'):
+
+    def in_date(year=2020, month=6, day=17, year2=2020, month2=6, day2=19, zone='Europe/Moscow'):
         # Весь список операций
         d1 = datetime(year, month, day, 0, 0, 0,
                       tzinfo=timezone(zone))  # timezone нужно указывать. Иначе - ошибка
@@ -179,7 +182,7 @@ class get_operation():
                     print(get.payload.name)
             print('--------------')
 
-    def dividend(month=1,year=2020,day=1,hour=0,minute=0,second=1,zone='Europe/Moscow'):
+    def dividend(month=1, year=2020, day=1, hour=0, minute=0, second=1, zone='Europe/Moscow'):
         # Весь список операций
         d1 = datetime(year, month, day, hour, minute, second,
                       tzinfo=timezone(zone))  # timezone нужно указывать. Иначе - ошибка
@@ -188,42 +191,75 @@ class get_operation():
         list_inst = get_instrument.all()
         list_name = {}
         for i in list_inst[4]:
-            list_name.update({i: [{'Dividend': [], 'TaxDividend': []}]})
+            if i == 'ЛЕГЕНДА выпуск 2':
+                list_name.update({i: [{'Coupon': [], 'TaxCoupon': []}]})
+            else:
+                list_name.update({i: [{'Dividend': [], 'TaxDividend': []}]})
             lll = str(i) + 'div_list'
             m = globals()[lll] = []
             kkk = str(i) + 'taxDiv_list'
             t = globals()[kkk] = []
-        for op in ops.payload.operations: # Перебираем операции
+        for op in ops.payload.operations:  # Перебираем операции
             if op.operation_type == 'Dividend':
                 a = client.market.market_search_by_figi_get(op.figi)
                 name = a.payload.name
                 if op.currency == 'USD':  # Если диведенты в $ переводим в рубли и добавляем в список
                     price_dollar = get_instrument.price_dollar()
                     price_in_rub = price_dollar * op.payment
-                    try:
-                        list_name[name][0][op.operation_type].append([op.date.strftime('%d.%m.%Y'), round(op.payment, 1), op.currency, round(int(price_in_rub), 1)])
-                    except:
-                        list_name.update({name: [{op.operation_type: [[op.date.strftime('%d.%m.%Y'), round(op.payment, 1), op.currency, round(int(price_in_rub), 1)]]}]})
+                    get_operation.list_update(list_name, name, op, price_in_rub)
                 else:
-                    get_operation.kabsfj(list_name, name, op)
+                    get_operation.list_update(list_name, name, op)
+            elif op.operation_type == 'Coupon':
+                a = client.market.market_search_by_figi_get(op.figi)
+                name = a.payload.name
+                get_operation.list_update(list_name, name, op)
+            elif op.operation_type == 'TaxCoupon':
+                a = client.market.market_search_by_figi_get(op.figi)
+                name = a.payload.name
+                get_operation.list_update(list_name, name, op)
             elif op.operation_type == 'TaxDividend':
                 a = client.market.market_search_by_figi_get(op.figi)
                 name = a.payload.name
-                get_operation.kabsfj(list_name, name, op)
+                get_operation.list_update(list_name, name, op)
             else:
                 continue
+        pprint(list_name)
+        print('----------------------------')
+        list_company_del, list_op_del = [], []
+        for i in list_name:
+            for m in list_name[i][0]:
+                if not list_name[i][0][m]:
+                    list_company_del.append(i)
+                    list_op_del.append(m)
+        for l, h in zip(list_company_del, list_op_del):
+            del list_name[l][0][h]
+        for a in list(list_name.keys()):
+            if not list_name[a][0]:
+                del list_name[a]
         pprint(list_name)
         return list_name
 
     @staticmethod
-    def kabsfj(list_name, name, op):
-        try:
-            list_name[name][0][op.operation_type].append([op.date.strftime('%d.%m.%Y'), round(op.payment, 1), op.currency])
-        except:
-            list_name.update({name: [{op.operation_type: [[op.date.strftime('%d.%m.%Y'), round(op.payment, 1), op.currency]]}]})
+    def list_update(list_name, name, op, price_in_rub=0):
+        if op.currency == 'USD':
+            try:
+                list_name[name][0][op.operation_type].append(
+                    [op.date.strftime('%d.%m.%Y'), round(op.payment, 1), op.currency, round(int(price_in_rub), 1)])
+            except:
+                list_name.update({name: [{op.operation_type: [
+                    [op.date.strftime('%d.%m.%Y'), round(op.payment, 1), op.currency, round(int(price_in_rub), 1)]]}]})
+        else:
+            try:
+                list_name[name][0][op.operation_type].append(
+                    [op.date.strftime('%d.%m.%Y'), round(op.payment, 1), op.currency])
+            except:
+                list_name.update(
+                    {name: [{op.operation_type: [[op.date.strftime('%d.%m.%Y'), round(op.payment, 1), op.currency]]}]})
 
 
 get_operation.dividend()
+#
+# get_operation.in_date()
 
 # get_operation.one(2)
 # get_operation.all(2020, 5, 25)
